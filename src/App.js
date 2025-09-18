051a5f971f55bce85dc33741e76fed21572d2155
Nitin Kumar <nitinrishab2000@gmail.com>
2025-09-18 20:16:37 +0530

    v1 created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useContext } from "react";
+import MessageContext from "./context/MessageContext";
 
 function App() {
-  return <div className="App"></div>;
+  const message = "Hello world";
+  const name = "Nitin";
+  const age = 25;
+
+  const messageDetail = {
+    message,
+    name,
+    age,
+  };
+  return (
+    <div className="App">
+      <MessageContext.Provider value={messageDetail}>
+        <ComponentA></ComponentA>
+      </MessageContext.Provider>
+    </div>
+  );
 }
 
+function ComponentA() {
+  return <ComponentB></ComponentB>;
+}
+
+function ComponentB() {
+  const message = useContext(MessageContext);
+  return (
+    <div className="App">
+      <h1>Message : {message.message}</h1>
+      <h1>Name : {message.name}</h1>
+      <h1>Age : {message.age}</h1>
+    </div>
+  );
+}
 export default App;
